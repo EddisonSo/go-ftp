00dfd9c545ea258a0b30924667a61309d6f7e545
eddison <eddisonso@gmail.com>
2025-03-21 02:08:39 -0400

    changed to singleton logger

--- internal/filehandler/filewriter.go
+++ internal/filehandler/filewriter.go
@@ -3,25 +3,27 @@ package filehandler
 import "bufio"
 import "os"
 import "fmt"
+import "log/slog"
 
 type Writer struct {
     Filename string
     Filereader *bufio.Writer
+    Logger *slog.Logger
 }
 
 func (w *Writer) Write(b []byte) (int, error) {
-    fmt.Println("Writing to file: ", w.Filename)
-    fmt.Println("Data: ", string(b))
+    w.Logger.Info("Writing to file: " + w.Filename)
+    w.Logger.Info("Data: " + fmt.Sprintf("%s", b))
     n, err := w.Filereader.Write(b)
     w.Filereader.Flush()
     return n, err
 }
 
-func NewFilewriter(filename string) (*Writer, error) {
+func NewFilewriter(filename string, logger *slog.Logger) (*Writer, error) {
     file, err := os.Create(filename)
     if err != nil {
 	return nil, err
     }
 
-    return &Writer{Filename:filename, Filereader:bufio.NewWriter(file)}, nil
+    return &Writer{Filename:filename, Filereader:bufio.NewWriter(file), Logger:logger}, nil
 }
