fe886e0e1afaa21870a889528ffb0b421c9d9ef7
eddison <eddisonso@gmail.com>
2025-03-21 02:08:22 -0400

    changed to singleton logger

--- internal/filehandler/filereader.go
+++ internal/filehandler/filereader.go
@@ -2,24 +2,28 @@ package filehandler
 
 import "bufio"
 import "os"
+import "log/slog"
 
 type Reader struct {
     Filename string
     Filereader *bufio.Reader
     File *os.File
+    Logger *slog.Logger
 }
 
 func (r *Reader) Read(b []byte) (int, error) {
-    return r.Filereader.Read(b)
+    n, err := r.Filereader.Read(b)
+    r.Logger.Info("Read from file: " + r.Filename)
+    return n, err
 }
 
-func NewFilereader(filename string) (*Reader, error) {
+func NewFilereader(filename string, logger *slog.Logger) (*Reader, error) {
     file, err := os.Open(filename)
     if err != nil {
 	return nil, err
     }
 
-    return &Reader{File:file, Filename:filename, Filereader:bufio.NewReader(file)}, nil
+    return &Reader{File:file, Filename:filename, Filereader:bufio.NewReader(file), Logger:logger}, nil
 }
 
 func (r *Reader) Getsize() (uint32, error) {
